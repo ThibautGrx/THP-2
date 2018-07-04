781c612fbe66be382993b1633437186c1aff2567
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2018-07-04 11:10:15 +0200

    edit validation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,5 @@
 class Lesson < ApplicationRecord
-  validates :title, :length, presence: true
+  validates :title, :description, presence: true
   validates :title, length: { maximum: 50 }
   validates :description, length: { maximum: 300 }
 end
