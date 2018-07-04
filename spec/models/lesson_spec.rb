6f1da23f0aa1b4578fe4c8d775611f46333cd4ce
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2018-07-04 11:46:46 +0200

    test validation

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -18,4 +18,11 @@ RSpec.describe Lesson, type: :model do
     expect(first_lesson.title).to eq(lesson.title)
     expect(first_lesson.description).to eq(lesson.description)
   end
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+
+  it { should validate_length_of(:title).is_at_most(50) }
+  it { should validate_length_of(:description).is_at_most(300) }
+
 end
