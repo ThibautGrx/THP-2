ff0cbb65ad6016e164bd9b924418756d9ea18c3f
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2018-07-04 11:12:13 +0200

    gem annotate + bundle install

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id          :uuid             not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Lesson < ApplicationRecord
   validates :title, :description, presence: true
   validates :title, length: { maximum: 50 }
