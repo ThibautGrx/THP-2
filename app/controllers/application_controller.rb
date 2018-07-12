35ad6700d7eaf11665d9b57efc76306cb9ee8187
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2018-07-12 16:56:48 +0200

    delet sentry config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,4 @@ class ApplicationController < ActionController::API
   def record_not_found(exception)
     render json: { errors: [{ detail: exception.message }] }, status: :not_found
   end
-
-  before_action :set_raven_context
-
-  private
-
-  def set_raven_context
-    Raven.user_context(id: session[:current_user_id]) # or anything else in session
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-  end
 end
