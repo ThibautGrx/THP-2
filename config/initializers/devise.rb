ae1d6a71b0e02edcd52beee1952f918eb6e5662c
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2018-07-15 22:12:26 +0200

    change email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # The e-mail address that mail will appear to be sent from
   # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
-  config.mailer_sender = "denis@thp2.co"
+  config.mailer_sender = "thibaut@thp2.co"
 
   # If using rails-api, you may want to tell devise to not use ActionDispatch::Flash
   # middleware b/c rails-api does not include it.
