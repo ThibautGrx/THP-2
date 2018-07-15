f5c89482cb2e2e75208faa997a22dd7057cf017d
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2018-07-15 22:38:31 +0200

    add error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  def fail
+    raise 'It fails !'
+  end
+
+  before_action :fail
+
   def record_not_found(exception)
     render json: { errors: [{ detail: exception.message }] }, status: :not_found
   end
