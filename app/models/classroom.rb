3be8284c4dbe10ea0b98ccc9bbc9466b783085af
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2018-07-25 14:12:52 +0200

    change classroom.students (instead of classroom.users)

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -13,15 +13,11 @@
 class Classroom < ApplicationRecord
   belongs_to :lesson
   has_many :invitations, dependent: :destroy
-  has_many :users, through: :invitations
+  has_many :students, through: :invitations, source: :user
   has_many :questions, dependent: :destroy
   has_many :ticked_steps, dependent: :destroy
   has_many :steps, through: :ticked_steps
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 300 }
-
-  def teacher
-    lesson.creator
-  end
 end
