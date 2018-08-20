7e0180b7f78501e75e24df286c31a6956fe61515
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2018-08-20 13:19:34 +0200

    correct routes test

--- config/routes.rb
+++ config/routes.rb
@@ -14,47 +14,47 @@ end
 
 # == Route Map
 #
-#                       Prefix Verb   URI Pattern                                                                              Controller#Action
-#             new_user_session GET    /auth/sign_in(.:format)                                                                  devise_token_auth/sessions#new
-#                 user_session POST   /auth/sign_in(.:format)                                                                  devise_token_auth/sessions#create
-#         destroy_user_session DELETE /auth/sign_out(.:format)                                                                 devise_token_auth/sessions#destroy
-#            new_user_password GET    /auth/password/new(.:format)                                                             devise_token_auth/passwords#new
-#           edit_user_password GET    /auth/password/edit(.:format)                                                            devise_token_auth/passwords#edit
-#                user_password PATCH  /auth/password(.:format)                                                                 devise_token_auth/passwords#update
-#                              PUT    /auth/password(.:format)                                                                 devise_token_auth/passwords#update
-#                              POST   /auth/password(.:format)                                                                 devise_token_auth/passwords#create
-#     cancel_user_registration GET    /auth/cancel(.:format)                                                                   devise_token_auth/registrations#cancel
-#        new_user_registration GET    /auth/sign_up(.:format)                                                                  devise_token_auth/registrations#new
-#       edit_user_registration GET    /auth/edit(.:format)                                                                     devise_token_auth/registrations#edit
-#            user_registration PATCH  /auth(.:format)                                                                          devise_token_auth/registrations#update
-#                              PUT    /auth(.:format)                                                                          devise_token_auth/registrations#update
-#                              DELETE /auth(.:format)                                                                          devise_token_auth/registrations#destroy
-#                              POST   /auth(.:format)                                                                          devise_token_auth/registrations#create
-#        new_user_confirmation GET    /auth/confirmation/new(.:format)                                                         devise_token_auth/confirmations#new
-#            user_confirmation GET    /auth/confirmation(.:format)                                                             devise_token_auth/confirmations#show
-#                              POST   /auth/confirmation(.:format)                                                             devise_token_auth/confirmations#create
-#          auth_validate_token POST   /auth/validate_token(.:format)                                                           devise_token_auth/token_validations#validate_token
-#           auth_password_edit POST   /auth/password/edit(.:format)                                                            devise_token_auth/passwords#edit
-# lesson_classroom_invitations GET    /lessons/:lesson_id/classrooms/:classroom_id/invitations(.:format)                       invitations#index
-#                              POST   /lessons/:lesson_id/classrooms/:classroom_id/invitations(.:format)                       invitations#create
-#  lesson_classroom_invitation GET    /lessons/:lesson_id/classrooms/:classroom_id/invitations/:id(.:format)                   invitations#show
-#                              PATCH  /lessons/:lesson_id/classrooms/:classroom_id/invitations/:id(.:format)                   invitations#update
-#                              PUT    /lessons/:lesson_id/classrooms/:classroom_id/invitations/:id(.:format)                   invitations#update
-#                              DELETE /lessons/:lesson_id/classrooms/:classroom_id/invitations/:id(.:format)                   invitations#destroy
-#            lesson_classrooms GET    /lessons/:lesson_id/classrooms(.:format)                                                 classrooms#index
-#                              POST   /lessons/:lesson_id/classrooms(.:format)                                                 classrooms#create
-#             lesson_classroom GET    /lessons/:lesson_id/classrooms/:id(.:format)                                             classrooms#show
-#                              PATCH  /lessons/:lesson_id/classrooms/:id(.:format)                                             classrooms#update
-#                              PUT    /lessons/:lesson_id/classrooms/:id(.:format)                                             classrooms#update
-#                              DELETE /lessons/:lesson_id/classrooms/:id(.:format)                                             classrooms#destroy
-#                      lessons GET    /lessons(.:format)                                                                       lessons#index
-#                              POST   /lessons(.:format)                                                                       lessons#create
-#                       lesson GET    /lessons/:id(.:format)                                                                   lessons#show
-#                              PATCH  /lessons/:id(.:format)                                                                   lessons#update
-#                              PUT    /lessons/:id(.:format)                                                                   lessons#update
-#                              DELETE /lessons/:id(.:format)                                                                   lessons#destroy
-#           rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
-#    rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
-#           rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
-#    update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
-#         rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+#                    Prefix Verb   URI Pattern                                                                              Controller#Action
+#          new_user_session GET    /auth/sign_in(.:format)                                                                  devise_token_auth/sessions#new
+#              user_session POST   /auth/sign_in(.:format)                                                                  devise_token_auth/sessions#create
+#      destroy_user_session DELETE /auth/sign_out(.:format)                                                                 devise_token_auth/sessions#destroy
+#         new_user_password GET    /auth/password/new(.:format)                                                             devise_token_auth/passwords#new
+#        edit_user_password GET    /auth/password/edit(.:format)                                                            devise_token_auth/passwords#edit
+#             user_password PATCH  /auth/password(.:format)                                                                 devise_token_auth/passwords#update
+#                           PUT    /auth/password(.:format)                                                                 devise_token_auth/passwords#update
+#                           POST   /auth/password(.:format)                                                                 devise_token_auth/passwords#create
+#  cancel_user_registration GET    /auth/cancel(.:format)                                                                   devise_token_auth/registrations#cancel
+#     new_user_registration GET    /auth/sign_up(.:format)                                                                  devise_token_auth/registrations#new
+#    edit_user_registration GET    /auth/edit(.:format)                                                                     devise_token_auth/registrations#edit
+#         user_registration PATCH  /auth(.:format)                                                                          devise_token_auth/registrations#update
+#                           PUT    /auth(.:format)                                                                          devise_token_auth/registrations#update
+#                           DELETE /auth(.:format)                                                                          devise_token_auth/registrations#destroy
+#                           POST   /auth(.:format)                                                                          devise_token_auth/registrations#create
+#     new_user_confirmation GET    /auth/confirmation/new(.:format)                                                         devise_token_auth/confirmations#new
+#         user_confirmation GET    /auth/confirmation(.:format)                                                             devise_token_auth/confirmations#show
+#                           POST   /auth/confirmation(.:format)                                                             devise_token_auth/confirmations#create
+#       auth_validate_token POST   /auth/validate_token(.:format)                                                           devise_token_auth/token_validations#validate_token
+#        auth_password_edit POST   /auth/password/edit(.:format)                                                            devise_token_auth/passwords#edit
+#         lesson_classrooms POST   /lessons/:lesson_id/classrooms(.:format)                                                 classrooms#create
+#                   lessons GET    /lessons(.:format)                                                                       lessons#index
+#                           POST   /lessons(.:format)                                                                       lessons#create
+#                    lesson GET    /lessons/:id(.:format)                                                                   lessons#show
+#                           PATCH  /lessons/:id(.:format)                                                                   lessons#update
+#                           PUT    /lessons/:id(.:format)                                                                   lessons#update
+#                           DELETE /lessons/:id(.:format)                                                                   lessons#destroy
+#     classroom_invitations POST   /classrooms/:classroom_id/invitations(.:format)                                          invitations#create
+#                classrooms GET    /classrooms(.:format)                                                                    classrooms#index
+#                 classroom GET    /classrooms/:id(.:format)                                                                classrooms#show
+#                           PATCH  /classrooms/:id(.:format)                                                                classrooms#update
+#                           PUT    /classrooms/:id(.:format)                                                                classrooms#update
+#                           DELETE /classrooms/:id(.:format)                                                                classrooms#destroy
+#               invitations GET    /invitations(.:format)                                                                   invitations#index
+#                invitation GET    /invitations/:id(.:format)                                                               invitations#show
+#                           PATCH  /invitations/:id(.:format)                                                               invitations#update
+#                           PUT    /invitations/:id(.:format)                                                               invitations#update
+#                           DELETE /invitations/:id(.:format)                                                               invitations#destroy
+#        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+# update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
--- spec/routing/classroom_routes_spec.rb
+++ spec/routing/classroom_routes_spec.rb
@@ -3,19 +3,17 @@ require 'rails_helper'
 describe "routing to classrooms" do
   let(:lesson) { create(:lesson) }
   let(:id) { lesson.id }
-  it "routes /lessons/:id/classrooms to classrooms#index" do
-    expect(get: "/lessons/#{id}/classrooms").to route_to(
+  it "routes /classrooms to classrooms#index" do
+    expect(get: "/classrooms").to route_to(
       controller: "classrooms",
       action: "index",
-      lesson_id: id
     )
   end
 
-  it "routes /lessons/:id/classrooms/:id to classrooms#show" do
-    expect(get: "/lessons/#{id}/classrooms/79cfcc41-edcb-4f5f-91c9-3fb9b3733509").to route_to(
+  it "routes /classrooms/:id to classrooms#show" do
+    expect(get: "/classrooms/79cfcc41-edcb-4f5f-91c9-3fb9b3733509").to route_to(
       controller: "classrooms",
       action: "show",
-      lesson_id: id,
       id: "79cfcc41-edcb-4f5f-91c9-3fb9b3733509"
     )
   end
@@ -28,20 +26,18 @@ describe "routing to classrooms" do
     )
   end
 
-  it "routes /lessons/:id/classrooms/:id to classrooms#update" do
-    expect(put: "/lessons/#{id}/classrooms/79cfcc41-edcb-4f5f-91c9-3fb9b3733509").to route_to(
+  it "routes /classrooms/:id to classrooms#update" do
+    expect(put: "/classrooms/79cfcc41-edcb-4f5f-91c9-3fb9b3733509").to route_to(
       controller: "classrooms",
       action: "update",
-      lesson_id: id,
       id: "79cfcc41-edcb-4f5f-91c9-3fb9b3733509"
     )
   end
 
-  it "routes /lessons/:id/classrooms/:id to classrooms#destroy" do
-    expect(delete: "/lessons/#{id}/classrooms/79cfcc41-edcb-4f5f-91c9-3fb9b3733509").to route_to(
+  it "routes /classrooms/:id to classrooms#destroy" do
+    expect(delete: "/classrooms/79cfcc41-edcb-4f5f-91c9-3fb9b3733509").to route_to(
       controller: "classrooms",
       action: "destroy",
-      lesson_id: id,
       id: "79cfcc41-edcb-4f5f-91c9-3fb9b3733509"
     )
   end
